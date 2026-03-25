4f472c1c963af4a0215aae1dd6f997ff7ad05b68
Leopold Mayer <leomayer@uw.edu>
2026-03-25 13:56:51 -0700

    imported files with facts on maschke's theorem and symmetric polynomials

--- GIT/Basic.lean
+++ GIT/Basic.lean
@@ -1,1 +1,8 @@
-def hello := "world"
+import Mathlib.RepresentationTheory.Maschke
+import Mathlib.RingTheory.MvPolynomial.Symmetric.FundamentalTheorem
+
+/- Ring of S_n invariants is given by symmetric polynomials -/
+#check MvPolynomial.esymmAlgEquiv
+
+/- The Reynolds operator for a finite group -/
+#check LinearMap.equivariantProjection
