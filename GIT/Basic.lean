21ce76078025c457032faeaa453b588dd9278ab8
Leopold Mayer <leomayer@uw.edu>
2026-04-06 17:21:07 -0700

    added some more defs

--- GIT/Basic.lean
+++ GIT/Basic.lean
@@ -3,10 +3,11 @@ import Mathlib.RingTheory.MvPolynomial.Symmetric.FundamentalTheorem
 import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Defs
 import Mathlib.RepresentationTheory.Rep
 import Mathlib.RepresentationTheory.Irreducible
+import Mathlib.RepresentationTheory.Invariants
 
 universe u
 
-variable {k : Type u} [Field k] (G : Type u) [Group G]
+variable (k : Type u) [Field k] (G : Type u) [Group G] [NeZero (Nat.card G : k)]
 
 /- Ring of S_n invariants is given by symmetric polynomials -/
 #check MvPolynomial.esymmAlgEquiv
@@ -19,7 +20,7 @@ a subgroup of `GL_n(k)` for some `n` -/
 def IsLinearAlgebraic := ∃ (n : Type*), ∃ _ : DecidableEq n, ∃ _ : Fintype n,
     ∃ H : Subgroup (GL n k), Nonempty (H ≃* G)
 
-open Monoid MonoidAlgebra Representation
+open Monoid MonoidAlgebra Representation Algebra
 
 /- the category of G-representations over k -/
 #check Rep k G
@@ -46,6 +47,32 @@ example {α : Type} (ι : α → Rep k G) : Rep k G := ∏ᶜ ι
 
 --expressing a represesntation is irreductible
 example (M : Rep k G) : Prop := IsIrreducible (Rep.ρ M)
+
+def ReynoldsOperator (M : Rep k G) := M →ₗ[k] (invariants (Rep.ρ M))
+
+variable (R : Type*) [CommRing R] [Algebra k R]
+
+/- A group `G` is linearly reductive over a field `k` if every finite dimensional representation
+   is semisimple
+-/
+class LinearlyReductive where
+  isLinearlyReductive : ∀ M : Rep k G, FiniteDimensional k M → IsSemisimpleRepresentation (Rep.ρ M)
+
+/- expressing that a finite group is -/
+example [Finite G] : LinearlyReductive k G := ⟨inferInstance⟩
+
+--expressing that a k-algebra is finitely generated
+#check FiniteType k R
+
+--expressing that R is a k-algebra with a G-action
+variable [MulSemiringAction G R] [SMulCommClass G k R]
+
+#check invariants
+
+#check FixedPoints.subalgebra k R G
+
+theorem FiniteType_invariants [FiniteType k R] [LinearlyReductive k G] :
+    FiniteType k (FixedPoints.subalgebra k R G) := sorry
 
 
 /- Trying to express what it means for a group to be linearly reductive -/
