30e9ef339e7f27772c81da21de9a9897a411a2f2
Jacob Boyce <jacobbboyce@gmail.com>
2026-04-13 16:45:52 -0700

    added comments to ouline proof strucuture for given k-algebra R with fg irrelevant ideal, R is fg

--- GIT/k_algebra_test.lean
+++ GIT/k_algebra_test.lean
@@ -12,10 +12,10 @@ open Algebra HomogeneousIdeal
 /- internally ℕ-graded `k`-algebra -/
 #check GradedAlgebra 𝒜
 
-/- irrelevant ideal `R₊` (kernel of degree-0 projection); Mathlib also writes `𝒜₊` locally -/
+/- irrelevant ideal `R₊` . Mathlib also writes `𝒜₊` locally -/
 #check irrelevant 𝒜
 
-/- as an ordinary ideal of `R` (e.g. for `Ideal.FG`) -/
+/- as an ordinary ideal of `R` (used for `Ideal.FG`) -/
 #check (irrelevant 𝒜).toIdeal
 
 /- homogeneous ideal, not a separate type for `R₊` -/
@@ -29,6 +29,21 @@ open Algebra HomogeneousIdeal
 
 /- degree-`0` part as `k`-algebra -/
 #check Algebra k (𝒜 0)
+
+/-
+HANDWRITTEN PROOF TO FOLLOW: https://math.gsu.edu/fenescu/commalglectures/lect20.pdf
+
+THRM:
+Let R be a positively graded R_0-algebra. Let x_1, ..., x_n be elements of
+positive degree. Then (x_1, ..., x_n) = bigoplus_{i=1}^∞ R_i ... only if {x_1, ..., x_n} generates R
+as an R_0-algebra.
+
+PROOF:
+...we proceed by induction on the degree of y. If deg y = 0,
+the proof is clear, so assume a positive degree. We have y ∈ (x_1, ..., x_n), so y = α_1x_1 +
+··· + α_nx_n, with α_i ∈ R, and if deg x_i = d_i, then α_i ∈ R_{deg y−d_i} . To finish, apply the
+induction hypothesis to the α_i’s.
+-/
 
 /-
 If `R₊` is finitely generated as an ideal and `𝒜 0` is finitely generated as a `k`-algebra
